bbc9b40da784df41ec6c3e5144c6e991157b05b5
zsc <robin.zhou0412@gmail.com>
2021-03-16 20:00:40 +0800

    0316

--- Manager.py
+++ Manager.py
@@ -1,40 +1,40 @@
 from Server import Server,VM
 from operator import attrgetter
+import sys
 class Manager():
-    def __init__(self,filepath='./training-1.txt') -> None:
+    def __init__(self) -> None:
         self.server_list = []
         self.server_count = 0
         self.sortflag = ''
         self.vm_list = []
         self.vm_count = 0
-        with open(filepath,'r') as f:
-            lines = f.readlines()
-            serverline = int(lines[0])
-            vmlines = int(lines[serverline+1])
-            readline = 1
-            for i in range(serverline):
-                server = lines[readline][1:-2].split(', ')    
-                self.server_list.append(Server(server[0],int(server[1]),int(server[2]),int(server[3]),int(server[4])))
-                self.server_count += 1
-                readline += 1
-                self.sortflag = ''
-            readline += 1
-            for i in range(vmlines):
-                vm = lines[readline][1:-2].split(', ')
-                self.vm_list.append(VM(vm[0],int(vm[1]),int(vm[2]),int(vm[3])))
-                self.vm_count += 1
-                readline += 1
-            self.days = int(lines[readline])
-            self.ops =[]
-            readline += 1
-            for i in range(self.days):
-                day =[]
-                ops_num = int(lines[readline])
-                readline += 1
-                for j in range(ops_num):
-                    day.append(lines[readline][1:-2].split(', '))
-                    readline += 1
-                self.ops.append(day)
+        
+        lines = sys.stdin.readline()
+        print(lines,type(lines))
+        serverline = int(lines)
+        # vmlines = int(lines[serverline+1])
+        for i in range(serverline):
+            a = sys.stdin.readline()
+            server = a[1:-2].split(', ')    
+            self.server_list.append(Server(server[0],int(server[1]),int(server[2]),int(server[3]),int(server[4])))
+            self.server_count += 1
+            self.sortflag = ''
+        vmlines = int(sys.stdin.readline())
+        for i in range(vmlines):
+            a = sys.stdin.readline()
+            vm = a[1:-2].split(', ')
+            self.vm_list.append(VM(vm[0],int(vm[1]),int(vm[2]),int(vm[3])))
+            self.vm_count += 1
+        self.days = int(sys.stdin.readline())
+        self.ops =[]
+
+        for i in range(self.days):
+            day =[]
+            ops_num = int(sys.stdin.readline())
+            for j in range(ops_num):
+                a = sys.stdin.readline()
+                day.append(a[1:-2].split(', '))
+            self.ops.append(day)
         # print(self.days)
         # print(self.ops[0])
 
@@ -57,7 +57,7 @@ class Manager():
             self.server_list.sort(key=attrgetter('hard_cost','energy_cost','cores','mems'))
             self.sortflag ='hc'
         for i in self.server_list:
-            if i.cores >cores and i.mems >mems :
+            if i.cores >=cores and i.mems >=mems :
                 return i
     
     def find_server_by_ec(self,cores:int,mems:int)->Server:
@@ -65,7 +65,7 @@ class Manager():
             self.server_list.sort(key=attrgetter('energy_cost','hard_cost','cores','mems'))
             self.sortflag ='ec'
         for i in self.server_list:
-            if i.cores >cores and i.mems >mems :
+            if i.cores >=cores and i.mems >=mems :
                 return i
 
 
@@ -110,7 +110,7 @@ class Manager():
         return 'There are {} servers, and {} virtual machines'.format(self.server_count,self.vm_count)
 
 if __name__ =='__main__':
-    manager = Manager('./training-1.txt')
+    manager = Manager()
     a = manager.find_vm_by_name('vmSQCQ3')
     b = manager.find_server_by_hc_list(200,200,5)
     print(a)
--- Scheduler.py
+++ Scheduler.py
@@ -27,6 +27,7 @@ class Scheduler():
         return len(self.free_server)
 
     def add_server_to_free(self,server:RunningServer):
+        purchase.append("{}:{}".format(server.name,server.id))
         self.free_server.append(server)
     
     def add_server_to_running(self,server:RunningServer):
@@ -49,23 +50,24 @@ class Scheduler():
         return -1
 
     def find_server_from_free_server(self,cores,mems) -> int:
+        self.sort_free_server_by_ec()
         for i in range(self.free_server_cnt):
             if self.free_server[i].cores >= cores and self.free_server[i].mems >= mems :
                 return i 
         return -1
     
     def find_vm_by_id(self,id:str) -> int:
-        for i in range(self.running_vm_cnt):
+        for i in range(self.running_vm_cnt-1,-1,-1):
             if self.running_vm[i].id == id :
                 return i
     
     def find_running_server_by_id(self,serverid):
-        for i in range(self.running_server_cnt):
+        for i in range(self.running_server_cnt-1,-1,-1):
             if self.running_server[i].id == serverid :
                 return i
 
     def find_free_server_by_id(self,serverid):
-        for i in range(self.free_server_cnt):
+        for i in range(self.free_server_cnt-1,-1,-1):
             if self.free_server[i].id == serverid :
                 return i
 
@@ -80,6 +82,7 @@ class Scheduler():
             return False
         self.running_vm[i].setrunningloc(serverid,node)
         self.running_server[j].setrunningnode(vmid,node)
+        bushu.append("{}:{}".format(serverid,node))
         return True
 
     def add_svm_from_free_server(self,vmid,serverid,freeloc):
@@ -94,6 +97,7 @@ class Scheduler():
         self.free_server[freeloc].setrunningnode(vmid,node)
         server = self.free_server.pop(freeloc)
         self.running_server.append(server)
+        bushu.append("{}:{}".format(serverid,node))
         return True
 
     def add_bsv_to_running_server(self,vmid,serverid,serverloc):
@@ -104,6 +108,8 @@ class Scheduler():
         self.free_server[serverloc].setrunningnode(vmid,node)
         server = self.free_server.pop(serverloc)
         self.running_server.append(server)
+        bushu.append("{}".format(serverid))
+        
 
     def addopt(self,vmname,vmid):
         vm = self.add_vm(RunningVM(self.manager.find_vm_by_name(vmname),vmid))
@@ -120,11 +126,17 @@ class Scheduler():
     def delvm(self,vm:RunningVM):
         serverid  = vm.running_server
         node = vm.running_node
-
+        serverloc = self.find_running_server_by_id(serverid)
+        
+        self.running_server[serverloc].delrunningnode(node)
+        
+        if self.running_server[serverloc].Ais_empty and self.running_server[serverloc].Bis_empty :
+            server = self.running_server.pop(serverloc)
+            self.free_server.append(server)
 
     def addbinode(self,vm:RunningVM):
         loc = self.find_server_from_free_server(vm.cores,vm.mems)
-        if loc >0 :
+        if loc >=0 :
             serverid = self.free_server[loc].id
             self.add_bsv_to_running_server(vm.id,serverid,loc)
         else:
@@ -133,14 +145,61 @@ class Scheduler():
 
     def addsnode(self,vm:RunningVM):
         runningloc = self.find_server_from_running_server(vm.cores,vm.mems)
-        if runningloc>0:
+        if runningloc>=0:
             serverid = self.running_server[runningloc].id
             self.add_svm_to_running_server(vm.id,serverid)
             return 
         freeloc = self.find_server_from_free_server(vm.cores,vm.mems)
-        if freeloc>0:
+        if freeloc>=0:
             serverid = self.free_server[freeloc].id
             self.add_svm_from_free_server(vm.id,serverid,freeloc)
         else:
-            self.add_server_to_free(RunningServer(self.manager.find_server_by_hc(vm.cores,vm.mems),self.server_cnt))
+            self.add_server_to_free(RunningServer(self.manager.find_server_by_hc(vm.cores*2,vm.mems*2),self.server_cnt))
             self.addsnode(vm)
+
+    # def daily_opt(self,day:int):
+    #     purchase = []
+    #     bushu = []
+    #     opt = self.manager.get_day_ops(day)
+    #     for i in range(len(opt)):
+    #         if i[0] == 'add':
+    #             self.addopt(i[i],i[2])
+    #         elif i[0] == 'del':
+    #             self.delopt(i[1])
+        
+if __name__ =='__main__':
+
+    manager = Manager()
+    scheduler = Scheduler(manager)
+    days = scheduler.manager.get_days()
+    ids = 0
+    dic ={}
+    for i in range(days):
+        purchase = []
+        bushu = []
+        opt = manager.get_day_ops(i)
+        for j in opt:
+            if j[0] == 'add':
+                scheduler.addopt(j[1],j[2])
+            elif j[0] =='del':
+                scheduler.delopt(j[1])
+        name = []
+        purchase.sort()
+        for i in purchase:
+            a = i.split(':')
+            name.append(a[0])
+            dic[a[1]] = ids
+            ids += 1
+        print("(purchase, {})".format(len(set(name))))
+        for i in set(name):
+            print("({}, {})".format(i,name.count(i)))
+        
+        print("(migratiion, 0)")
+        for i in bushu:
+            if ':' not in i:
+                print("({})".format(dic[i]))
+            else:
+                a = i.split(':')
+                print("({}, {})".format(dic[a[0]],a[1]))
+
+    # print(scheduler.running_server[0].runningatA,scheduler.running_server[0].runningatB)
